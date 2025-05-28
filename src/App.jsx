e612b6ace6570c4ff21565935c2b16ae373b2dfb
Dharita Patel <dharitapatel.098@gmail.com>
2025-05-28 15:33:31 +0530

    change in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function ProtectedRoute({ element }) {
   
       try {
         const res = await axios.get(
-          `http://localhost:3001/api/health-data/${user._id}`
+          `${import.meta.env.VITE_BACKEND_URL}/api/health-data/${user._id}`
         );
   
         setalldata(res.data);
@@ -133,7 +133,7 @@ function AppContent() {
 
       try {
         const res = await axios.get(
-          `http://localhost:3001/api/health-data/${user._id}`
+          `${import.meta.env.VITE_BACKEND_URL}/api/health-data/${user._id}`
         );
 
         setalldata(res.data);
